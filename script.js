9f3b74ecd872f5e03a23a799175fe2a0705a146f
Jay Peek <73311799+ChefJayPeek@users.noreply.github.com>
2020-11-13 14:18:32 -0600

    possible correct search variable

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 
     //build the query url
 
-    // var search = //get code from html with jQuery
-    var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?Q=covid&api-key=GfMFFaeegwYa9U8WP04Q08tGCFW0G8PR";
+     var search = $("#exampleInputEmail1"); //get code from html with jQuery
+    var queryUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?Q=" + search + "&api-key=GfMFFaeegwYa9U8WP04Q08tGCFW0G8PR";
     $.ajax({
         url: queryUrl,
         method: "GET"
@@ -12,8 +12,9 @@
     
     })
     .then(function(response) {
-        console.log(response);
-
+        //console.log(response);
+        var headline = response.headline.main;
+        var article = response.lead_paragraph
 
 
     })
